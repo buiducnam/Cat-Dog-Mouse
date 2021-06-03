6ec57cc3f1c6334e548cff37fe46fea461d9ef55
BuiDucNam <namkhang714@gmail.com>
2021-06-03 16:55:58 +0700

    Add methods eat for Dog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,11 @@
 function Dog(name, age){
     this.name = name;
     this.age = age;
+    this.stomach = [];
+}
+
+Dog.prototype.eat = function(cat){
+    this.stomach.push(cat)
 }
 
 module.export = Dog;
