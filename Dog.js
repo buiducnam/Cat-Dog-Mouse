538b9e14a14171957eeedd9a8def319127a8c19c
BuiDucNam <namkhang714@gmail.com>
2021-06-03 21:00:47 +0700

    Change two file

--- Dog.js
+++ Dog.js
@@ -1,6 +1,11 @@
+const chalk = require('chalk');
 function Dog(name, age){
     this.name = name;
     this.age = age;
+}
+
+Dog.prototype.eat = function(){
+    console.log(`My name is ${chalk.red(this.name)}`);
 }
 
 module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,4 @@
+const Dog = require('./Dog');
+
+var micky = new Dog('Micky', 12);
+micky.eat();
