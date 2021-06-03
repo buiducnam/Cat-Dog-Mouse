8427f6a90cf8731da98d97aaf9b2cff3fd050ed5
BuiDucNam <namkhang714@gmail.com>
2021-06-03 18:33:54 +0700

    Change Dog file

--- Dog.js
+++ Dog.js
@@ -1,11 +1,6 @@
 function Dog(name, age){
     this.name = name;
     this.age = age;
-    this.stomach = [];
-}
-
-Dog.prototype.eat = function(cat){
-    this.stomach.push(cat)
 }
 
 module.export = Dog;
