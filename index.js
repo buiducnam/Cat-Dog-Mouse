184bc0d198596e8963478bfbde0e4dca6efa8b09
BuiDucNam <namkhang714@gmail.com>
2021-06-04 17:07:20 +0700

    Add cat - eat -mouse

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const Dog = require('./Dog');
+const Mouse = require('./Mouse');
+const Cat = require('./Cat');
 
-var micky = new Dog('Micky', 12);
-micky.eat();
+var meo = new Cat('Meo  Meo' , 10);
+var mouse = new Mouse("Chuot");
+// console.log(meo);
+meo.eat(mouse);
+console.log(meo)
